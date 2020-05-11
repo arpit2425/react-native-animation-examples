bb475ec65736e98e3154c987c28b414f434d8eea
Arpit Trivedi <arpittrivedi2425@gmail.com>
2020-05-11 20:02:42 +0530

    previous card returning animation"

--- Screens/Inshot.js
+++ Screens/Inshot.js
@@ -19,6 +19,7 @@ class Inshot extends Component {
   constructor(props) {
     super(props);
     this.position = new Animated.ValueXY();
+    this.swipedCardPosition = new Animated.ValueXY({ x: 0, y: -Height });
     this.state = {
       currentItem: 0,
     };
@@ -27,10 +28,22 @@ class Inshot extends Component {
     this.PanResponder = PanResponder.create({
       onStartShouldSetPanResponder: () => true,
       onPanResponderMove: (e, gesture) => {
-        this.position.setValue({ x: 0, y: gesture.dy });
+        if (gesture.dy > 0 && this.state.currentItem > 0) {
+          this.swipedCardPosition.setValue({ x: 0, y: -Height + gesture.dy });
+        } else {
+          this.position.setValue({ x: 0, y: gesture.dy });
+        }
       },
       onPanResponderRelease: (e, g) => {
-        if (-g.dy > 50 && -g.vy > 0.7) {
+        if (this.state.currentItem > 0 && g.dy > 50 && g.vy > 0.7) {
+          Animated.timing(this.swipedCardPosition, {
+            toValue: { x: 0, y: 0 },
+            duration: 400,
+          }).start(() => {
+            this.setState({ currentItem: this.state.currentItem - 1 });
+            this.swipedCardPosition.setValue({ x: 0, y: -Height });
+          });
+        } else if (-g.dy > 50 && -g.vy > 0.7) {
           Animated.timing(this.position, {
             toValue: { x: 0, y: -Height },
             duration: 400,
@@ -39,9 +52,14 @@ class Inshot extends Component {
             this.position.setValue({ x: 0, y: 0 });
           });
         } else {
-          Animated.spring(this.position, {
-            toValue: { x: 0, y: 0 },
-          }).start();
+          Animated.parallel([
+            Animated.spring(this.position, {
+              toValue: { x: 0, y: 0 },
+            }),
+            Animated.spring(this.swipedCardPosition, {
+              toValue: { x: 0, y: -Height },
+            }),
+          ]).start();
         }
       },
     });
@@ -49,7 +67,62 @@ class Inshot extends Component {
   renderCards = () => {
     return articles
       .map((item, i) => {
-        if (i < this.state.currentItem) {
+        if (i == this.state.currentItem - 1) {
+          return (
+            <Animated.View
+              key={item.id}
+              style={this.swipedCardPosition.getLayout()}
+              {...this.PanResponder.panHandlers}
+            >
+              <View
+                style={{
+                  flex: 1,
+                  position: "absolute",
+                  height: Height,
+                  width: Width,
+                  backgroundColor: "white",
+                }}
+              >
+                <View style={{ flex: 2, backgroundColor: "black" }}>
+                  <Image
+                    source={articles[i].uri}
+                    style={{ flex: 1, height: null, width: null }}
+                  ></Image>
+                </View>
+                <View style={{ flex: 3, padding: 5 }}>
+                  <Text>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Etiam sed tortor nec elit consectetur elementum vel nec
+                    justo. Duis porttitor nunc felis. Nullam auctor iaculis leo
+                    nec ornare. Aliquam augue nunc, gravida sit amet nunc eget,
+                    vehicula consequat ligula. Sed commodo, lorem vel volutpat
+                    condimentum, risus dui vestibulum ipsum, non malesuada dolor
+                    augue pharetra odio. Morbi eu consequat magna. Aliquam velit
+                    augue, porttitor eu bibendum congue, aliquam nec nibh. Cras
+                    dignissim eget mi sed faucibus. Quisque et imperdiet est.
+                    Nam bibendum eros mi, nec venenatis sem convallis sit amet.
+                    Nunc a sem ut turpis tincidunt sagittis non non est. Quisque
+                    viverra bibendum mauris. Mauris volutpat consectetur
+                    condimentum. Maecenas ultricies ex vel elementum mattis.
+                    Vestibulum eget magna quis nisi placerat rutrum. Integer
+                    lectus dolor, bibendum vel molestie eget, blandit a dolor.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Etiam sed tortor nec elit consectetur elementum vel nec
+                    justo. Duis porttitor nunc felis. Nullam auctor iaculis leo
+                    nec ornare. Aliquam augue nunc, gravida sit amet nunc eget,
+                    vehicula consequat ligula. Sed commodo, lorem vel volutpat
+                    condimentum, risus dui vestibulum ipsum, non malesuada dolor
+                    augue pharetra odio. Morbi eu consequat magna. Aliquam velit
+                    augue, porttitor eu bibendum congue, aliquam nec nibh. Cras
+                    dignissim eget mi sed faucibus. Quisque et imperdiet est.
+                    Nam bibendum eros mi, nec venenatis sem convallis sit amet.
+                    Nunc a
+                  </Text>
+                </View>
+              </View>
+            </Animated.View>
+          );
+        } else if (i < this.state.currentItem) {
           return null;
         }
         if (i == this.state.currentItem) {
